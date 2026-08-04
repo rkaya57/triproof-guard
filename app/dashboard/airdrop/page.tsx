2970362ce3fb115ef2b832be05d8e8d2da1dd48e
Nectar Hive | NECT <rkaya57.77@gmail.com>
2026-08-04 20:28:14 +0300

    Use reliable airdrop task status client

--- app/dashboard/airdrop/page.tsx
+++ app/dashboard/airdrop/page.tsx
@@ -1,5 +1,5 @@
-import { AirdropTasksClient } from "@/components/airdrop/airdrop-tasks-client"
+import { AirdropTasksClientV2 } from "@/components/airdrop/airdrop-tasks-client-v2"
 
 export default function AirdropTasksPage() {
-  return <AirdropTasksClient />
+  return <AirdropTasksClientV2 />
 }
